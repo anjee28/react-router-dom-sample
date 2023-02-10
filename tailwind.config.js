f2e2db5966629e54631eb636049874a8f14e474f
Mellhabib Angni <anjee28.twoeight@gmail.com>
2023-02-11 06:15:14 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
-
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {},
+	theme: {
+		extend: {},
+	},
 	plugins: [],
 };
